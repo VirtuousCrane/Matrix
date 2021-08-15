f50de242ffc3552117e6fb76cf449f011d0010e1
Chayut Liewlom <ray.daroonpat.2020@gmail.com>
2021-08-15 22:41:27 +0700

    Added room functionalities (#1)
    * Restructured Code
    
    * Restructured More Code
    
    * Added functions for checking the room's state and send an event to the room

--- src/connection.py
+++ src/connection.py
@@ -1,5 +1,6 @@
 import requests;
 import src.misc as misc;
+import src.error as error;
 from typing import *;
 
 def login(username: str, password: str) -> dict[str, str]:
@@ -23,7 +24,7 @@ def login(username: str, password: str) -> dict[str, str]:
     );
 
     res = res.json();
-    misc.raise_matrix_error(res);
+    error.raise_connection_error(res);
 
     return res;
 
@@ -53,7 +54,7 @@ def register_auth(username: str, password: str, session: str) -> dict[str, str]:
     );
 
     res = res.json();
-    misc.raise_matrix_error(res);
+    error.raise_connection_error(res);
 
     return res;
 
@@ -76,8 +77,74 @@ def register(username: str, password: str) -> dict[str, str]:
     );
 
     res = res.json();
-    misc.raise_matrix_error(res);
+    error.raise_connection_error(res);
     res = register_auth (username, password, res["session"]);
 
     return res;
 
+def create_room(access_token: str) -> dict[str, str]:
+    """Creates a new Matrix room
+
+    Parameters
+    ----------
+    access_token: str
+        The access token of the user
+    """
+
+    res = requests.post (
+        "http://localhost:8008/_matrix/client/r0/createRoom" +
+        "?access_token=" +
+        access_token,
+        json = {
+        },
+    );
+
+    res = res.json();
+    error.raise_connection_error (res);
+
+    return res;
+
+def get_room_state(room_id: str, access_token: str) -> dict[str, str]:
+    """Gets the room state
+
+    Parameters
+    ----------
+    room_id: str
+        The room's id
+    access_token: str
+        The access token we have previously acquired from logging in
+    """
+
+    url = f"http://localhost:8008/_matrix/client/r0/rooms/{room_id}/state?access_token={access_token}";
+    res = requests.get (url);
+    res = res.json();
+    error.raise_connection_error (res);
+
+    return res;
+
+def send_room_event(
+        room_id: str,
+        event_type: str,
+        state_key: str,
+        access_token: str
+    ) -> dict[str, str]:
+    """Sends a state event into the room
+
+    Parameters
+    ----------
+    room_id: str
+        The room's id
+    event_type: str
+        The type of event we want to fire into the room
+    state_key: str
+        The state key
+    access_token: str
+        The access token we have previously acquired from logging in
+    """
+
+    url = f"http://localhost:8008/_matrix/client/r0/rooms/{room_id}/state/{event_type}/{state_key}?access_token={access_token}";
+    res = requests.put (url, json = { "baz": "qux" });
+    res = res.json();
+    error.raise_connection_error (res);
+
+    return res;
--- src/misc.py
+++ src/misc.py
@@ -1,44 +1,75 @@
-from typing import *
-
-class Error(Exception):
-    """Base Class for exceptions"""
-    pass
-
-class ConnectionError(Error):
-    """Raised when there is a Connection Error"""
-    pass
+from src.error import ConnectionError, InputError;
+from typing import *;
+import src.connection as connection;
+import sys;
 
 def get_user_choice() -> int:
     """Returns the choice of the user"""
 
-    print("What are you going to do?")
-    print("(1) Register");
-    print("(2) Login   ");
-    print("(3) Quit    ");
+    print ("What are you going to do?")
+    print ("(1) Register   ");
+    print ("(2) Login      ");
+    print ("(3) Create Room");
+    print ("(4) Get Room State");
+    print ("(5) Send state event");
+    print ("(6) Quit       ");
 
     user_input = int(input("Your Choice: "));
 
-    if (user_input > 3 or user_input < 1):
-        raise Exception("Wrong Input");
+    if (user_input > 6 or user_input < 1):
+        raise InputError("Wrong Input");
 
     return user_input;
 
 def get_user_pwd() -> Tuple[str, str]:
     """Gets the username and password of the user"""
 
-    username = input("Enter username: ").strip();
-    pwd      = input("Enter password: ").strip();
+    username = input ("Enter username: ").strip();
+    pwd      = input ("Enter password: ").strip();
 
     return (username, pwd);
 
-def raise_matrix_error(result_dict: dict[str, str]):
-    """Raises an error if there is one
+def execute_user_choice(user_choice: int) -> dict[str, str]:
+    """Executes the choice of the user
 
     Parameters
     ----------
-    result_dict: dict[str, str]
-        The dictionary that comes from making a request
+    user_choice: int
+        The choice of the user
+        (1) Register
+        (2) Login
+        (3) Create Room
+        (4) Get Room State
+        (5) Exit
     """
 
-    if ("errcode" in result_dict):
-        raise ConnectionError(f"Error: {result_dict['errcode']}");
+    if (user_choice == 6):
+        sys.exit();
+
+    username, pwd = get_user_pwd();
+
+    if (user_choice == 1):
+        res = connection.register (username, pwd);
+    else:
+        res = connection.login (username, pwd);
+        access_token = res["access_token"];
+
+        if (user_choice == 3):
+            res = connection.create_room (access_token);
+
+        elif (user_choice == 4):
+            room_id = input ("Please enter room id: ");
+            res = connection.get_room_state (room_id, access_token);
+
+        elif (user_choice == 5):
+            room_id = input ("Please enter room id: ");
+            event_type = input ("Please enter event type: ");
+            state_key = input ("Please enter state key: ");
+            res = connection.send_room_event (
+                room_id,
+                event_type,
+                state_key,
+                access_token
+            );
+
+    return res;
